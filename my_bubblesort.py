172c12ff02a36576133f68768f2c1eb7dbf4a6ab
ArithmeticJia <1097197237@qq.com>
2020-01-31 20:38:03 +0800

    add bubblesort

--- my_bubblesort.py
+++ my_bubblesort.py
@@ -8,17 +8,21 @@ def bubble_sort_plus(nums):
                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
                 flag = True
         if not flag:
-            return nums  # 这里代表计算机偷懒成功 (〃'▽'〃)
+            return nums
 
 
 def bubble_sort(nums):
+    # n个数排序
+    # 排序n-1次
     for i in range(len(nums)-1):
         for j in range(len(nums)-i-1):
             if nums[j] > nums[j+1]:
                 nums[j], nums[j+1] = nums[j+1], nums[j]
+        print('第{}趟结果{}'.format(i, nums))
     return nums
 
 
 if __name__ == '__main__':
-    li = [4, 2, 5, 4, 5, 12, 0, 2, 3, 1, 1, 7, 4]
-    print(bubble_sort(li))
+    # li = [4, 2, 5, 4, 5, 12, 0, 2, 3, 1, 1, 7, 4]
+    li = [3, 4, 1, 2]
+    print('最后结果{}'.format(bubble_sort(li)))
