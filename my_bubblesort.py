09c0032897f1c497f7041f4b7d101bc5ceaedf7f
ArithmeticJia <1097197237@qq.com>
2020-01-31 19:23:34 +0800

    add bubblesort

--- my_bubblesort.py
+++ my_bubblesort.py
@@ -1,25 +1,24 @@
-class BublleSort():
+def bubble_sort_plus(nums):
 
-    def bubble_sort(self,nums):
-        for i in range(len(nums)-1):
-            for j in range(len(nums)-i-1):
-                if nums[j] > nums[j+1]:
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-        return nums
+    for i in range(len(nums) - 1):
+        flag = False  # 改进后的冒泡，设置一个交换标志位
+        for j in range(len(nums) - i - 1):
 
-    def bubble_sort_plus(self, nums):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                flag = True
+        if not flag:
+            return nums  # 这里代表计算机偷懒成功 (〃'▽'〃)
 
-        for i in range(len(nums) - 1):
-            flag = False  # 改进后的冒泡，设置一个交换标志位
-            for j in range(len(nums) - i - 1):
 
-                if nums[j] > nums[j + 1]:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
-                    flag = True
-            if not flag:
-                return nums  # 这里代表计算机偷懒成功 (〃'▽'〃)
+def bubble_sort(nums):
+    for i in range(len(nums)-1):
+        for j in range(len(nums)-i-1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums
 
 
 if __name__ == '__main__':
     li = [4, 2, 5, 4, 5, 12, 0, 2, 3, 1, 1, 7, 4]
-    print(BublleSort().bubble_sort(li))
+    print(bubble_sort(li))
