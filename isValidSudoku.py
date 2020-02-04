c66029318680428368c6f981740be08bdacf09a2
ArithmeticJia <1097197237@qq.com>
2020-02-04 16:43:46 +0800

    add isValidSudoku

---
+++ isValidSudoku.py
@@ -0,0 +1,47 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        # 行
+        rows = [{} for i in range(9)]
+        # 列
+        columns = [{} for i in range(9)]
+        # 每一小格
+        boxes = [{} for i in range(9)]
+
+
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num != '.':
+                    num = int(num)
+                    # 第几个box
+                    box_index1 = (i // 3) * 3 + j // 3
+                    box_index = i // 3 + j // 3 + 2
+                    # keep the current cell value
+                    rows[i][num] = rows[i].get(num, 0) + 1
+                    columns[j][num] = columns[j].get(num, 0) + 1
+                    boxes[box_index][num] = boxes[box_index].get(num, 0) + 1
+
+                    # check if this value has been already seen before
+                    if rows[i][num] > 1 or columns[j][num] > 1 or boxes[box_index][num] > 1:
+                        return False
+        return True
+
+
+if __name__ == '__main__':
+    board = [
+        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+    ]
+
+    print(Solution().isValidSudoku(board))
--- my_quickort.py
+++ my_quickort.py
@@ -1,7 +1,7 @@
 def quick_sort(list, start, end):
     # 递归的退出条件
     if start >= end:
-        return
+        return list
     # 设置起始元素为要寻找位置的基准元素
     mid = list[start]
     # left为序列左边的由左向右移动的游标
@@ -9,13 +9,13 @@ def quick_sort(list, start, end):
     # right为序列右边的由右向左移动的游标
     right = end
     while left < right:
-        # 如果left和right未重合，right指向的元素不比基准元素小，
+        # 从右边开始查找大于参考点的值
         # 则right向左移动
         while left < right and list[right] >= mid:
             right -= 1
         # 将right指向的元素放到left的位置上
         list[left] = list[right]
-        # 如果left于right未重合，left指向的元素比基准元素小
+        # 从左边开始查找小于参考点的值
         # 则left向右移动
         while left < right and list[left] < mid:
             left += 1
@@ -25,6 +25,7 @@ def quick_sort(list, start, end):
     # 退出循环后，left与right重合，此时所指位置为基准元素的正确位置
     # 将基准元素放到该位置
     list[left] = mid
+    print(list)
     # 对基准元素左边的子序列进行快速排序
     quick_sort(list, start, left-1)
     # 对基准元素右边的子序列进行快速排序
