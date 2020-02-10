254f127448cfcde9c3c0953a08f27a14b9725455
ArithmeticJia <1097197237@qq.com>
2020-02-10 16:05:15 +0800

    add minSteps.py

--- minSteps.py
+++ minSteps.py
@@ -1,14 +1,17 @@
 class Solution(object):
+    # 将一个数从2除到N - 1
+    # 遇到能整除的就不是素数，反之就是
     def minSteps(self, n):
         ans = 0
         d = 2
         while n > 1:
             while n % d == 0:
                 ans += d
+                print(d)
                 n /= d
             d += 1
         return ans
 
 
 if __name__ == '__main__':
-    print(Solution().minSteps(3))
+    print(Solution().minSteps(20))
