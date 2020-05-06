c910c3304f39a9e398c8a1b4b54199f9f01f7de9
ArithmeticJia <1097197237@qq.com>
2020-05-06 21:04:27 +0800

    add learn py

--- groupThePeople.py
+++ groupThePeople.py
@@ -1,17 +1,24 @@
 from collections import defaultdict
 import collections
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         # 初始化字典
         # value是list
-        groups = collections.defaultdict(list)
+        # groups = collections.defaultdict(list)
+        # for i, _id in enumerate(groupSizes):
+        #     groups[_id].append(i)
+        groups = {}
         for i, _id in enumerate(groupSizes):
-            groups[_id].append(i)
+            # 键存在返回值
+            # 键不存在返回默认的值
+            groups.setdefault(_id, []).append(i)
         # {3: [0, 1, 2, 3, 4, 6], 1: [5]})
         # 粗分组一下
+        print(groups)
         ans = list()
         for gsize, users in groups.items():
             for it in range(0, len(users), gsize):
