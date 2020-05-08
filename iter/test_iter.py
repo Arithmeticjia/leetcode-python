1bc61e10aa69151be5d1fd53099d48421c0ce3a5
ArithmeticJia <1097197237@qq.com>
2020-05-08 15:06:16 +0800

    add learn iter and generator

---
+++ iter/test_iter.py
@@ -0,0 +1,16 @@
+import sys
+
+list = [1, 2, 3, 4]
+
+it = iter(list)
+
+# 常规for语句进行遍历
+# for i in it:
+#     print(i, end="")
+
+# next()函数
+while True:
+    try:
+        print(next(it))
+    except StopIteration:
+        sys.exit()
