e3b91e489baccbfb02af9ec8b1d5d3999dac39cc
ArithmeticJia <1097197237@qq.com>
2020-05-15 09:46:30 +0800

    add asyncio and yield

---
+++ pyasyncio/test_asyncio.py
@@ -0,0 +1,12 @@
+import asyncio
+
+@asyncio.coroutine
+def get_body(i):
+    print(f'start{i}')
+    yield from asyncio.sleep(1)
+    print(f'start{i}')
+
+loop = asyncio.get_event_loop()
+tasks = [get_body(i) for i in range(5)]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
